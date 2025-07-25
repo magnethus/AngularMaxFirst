3225dcf917e5d7fd3ae6975397ab6816fd10bee2
System Administrator <root@MacBook-Pro-de-Alex.local>
2025-07-25 14:01:19 -0400

    adding signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 import { UserInputComponent } from './user-input/user-input.component';
@@ -12,14 +12,14 @@ import { InvestmentInput } from './investment-input.model';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  resultsData?: {
+  resultsData = signal< {
         year: number;
         interest: number;
         valueEndOfYear: number;
         annualInvestment: number;
         totalInterest: number;
-        totalAmountInvested: number; 
-  }[];
+        totalAmountInvested: number;
+    }[] | undefined >(undefined);
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } =
@@ -43,6 +43,6 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    this.resultsData = annualData; 
+    this.resultsData.set(annualData); 
   }
 }
